0c33403830eac6ac6e2ea1f42b8dfbba64e15929
Marco Almeida <marcoalmeida@Marcos-MacBook-Pro.local>
2016-11-26 19:34:44 -0500

    HW

--- Mission-Briefing/Mission Briefing/MissionBriefingViewController.m
+++ Mission-Briefing/Mission Briefing/MissionBriefingViewController.m
@@ -19,7 +19,7 @@
 
 @end
 
-@implementation ViewController
+@implementation MissionBriefingViewController
 
 - (void)viewDidLoad
 {
@@ -30,11 +30,11 @@
     //    Hint: there is a string literal that represents empty
     //
     
-    [self.agentNameTextField setText: @"" ];
+    self.agentNameTextField.text = @"" ;
     //<#what goes here?#>];
-    self.greetingLabel.text = @"" ];
+    self.greetingLabel.text = @"" ;
     // <#how about here?#>;
-    self.missionBriefingTextView.text = @"" ];
+    self.missionBriefingTextView.text = @"" ;
     //<#and here?#>;
 }
 
@@ -46,81 +46,41 @@
 
 - (IBAction)authenticateAgent:(UIButton *)sender
 {
-    // This will cause the keyboard to dismiss when the authenticate button is tapped
-    if ([self.agentNameTextField isFirstResponder])
+    NSString *agentName = self.agentNameTextField.text;
+    NSString *agentPassWordName = self.agentPasswordTextField.text;
+    NSArray *subStrings = [agentName componentsSeparatedByString:@" "];
+    if ([agentName length] == 0)
+      {
+      self.greetingLabel.text = @"Please provide your Login Name";
+      self.view.backgroundColor = [UIColor redColor];
+      return;
+      }
+    if ([agentPassWordName length] == 0)
     {
-        [self.agentNameTextField resignFirstResponder];
+        self.greetingLabel.text = @"Please provide your password";
+        self.view.backgroundColor = [UIColor redColor];
+        return;
     }
-    
-    //
-    // 2. Check whether there is text in BOTH the name and password textfields
-    //
-    if (self.agentNameTextField && self.agentPasswordTextField)
-//        if (<#the name textfield should go here#> && <#the password textfield should go here#>)
-
+    NSInteger size = [subStrings count];
+    if (size > 1)
     {
-        //
-        // 3. The greetingLabel needs to be populated with the the string "Good evening, Agent #", where # is the last name of
-        //    the agent logging in. The agent's full name is listed in the text field, but you need to pull out just the last
-        //    name. Open the Apple Documentation and go to the page for NSString. There is a section in the left called "Dividing
-        //    Strings". You should be able to find a method that allows you to break up a string using a delimiter. In our case,
-        //    the delimiter would be a space character.
-        //
+        NSString *agentLastName = [subStrings objectAtIndex:1];
+        //NSString *agentFirstName = [subStrings objectAtIndex:0];
+        self.greetingLabel.text = [NSString stringWithFormat:@"Good evening, Agent: %@",agentLastName];
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"MissionBriefing" ofType:@"txt"];
+        NSString *str=[NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
+        //    NSString *str = @"This is a string";
+        str = [str stringByReplacingOccurrencesOfString:@"%@" withString: agentLastName];
+        self.missionBriefingTextView.text = str;
+        self.view.backgroundColor = [UIColor greenColor];
         
-        NSString *agentName = self.agentNameTextField.text;
-        // Additional step(s) to remove only the last name
-        NSArray *agentLastName = [agentName componentsSeparatedByString:@" "];
-
-        self.greetingLabel.text = "Good evening, Agent: %@" Self.agentLastName.text
-        //<#look at #3 above for what text belongs here#>;
-        
-
-        //
-        // 4. The mission briefing textview needs to be populated with the briefing from HQ, but it must also include the last
-        //    name of the agent that logged in. You will notice in the text a "%@" string after the word "Agent". This
-        //    instructs the system to replace the "%@" with an actual value at runtime. Perhaps you could use the text in the
-        //    textfield to get the agent's last name.
-        //
-        //    Set the textview text property to the paragraph in "MissionBriefing.txt"
-        //
-        
-        self.missionBriefingTextView.text = [NSString stringWithFormat: %@,Self.agentLastName.text];
-        //<#look in missionbriefing.txt for what goes here#>
-        //,<#what needs to be placed inside the preceding text?#>];
-        
-        //
-        // 5. The view's background color needs to switch to green to indicate a successful login by the agent.
-        //
-        //    The color's RGB value is Red: 0.585, Green: 0.78, Blue: 0.188 with an alpha of 1. There is a class method on the
-        //    UIColor class that returns a color object with custom defined RGBA values. Open the documentation and look for a
-        //    method on UIColor that can take red, green, blue and alpha values as arguments.
-        //
-        //    Once you have the color object, you should be able to set the view's background color to this object.
-        //
-        + (UIColor *)colorWithRed:(0.585)red green:(0.78)green blue:(0.188)blue alpha:(1)alpha;
-        
-        UIColor *authenticatedBackgroundColor = green;
-        // Additional step to set the above color object to self.view's background color
-        self.view.backgroundColor = self.authenticatedBackgroundColor;
-        //<#how do we use the above color?#>;
     }
     else
     {
-        //
-        // 6. The view's background color needs to switch to red to indicate a failed login by the agent.
-        //
-        //    The color's RGB value is Red: 0.78, Green: 0.188, Blue: 0.188 with an alpha of 1. There is a class method on the
-        //    UIColor class that returns a color object with custom defined RGBA values. Open the documentation and look for a
-        //    method on UIColor that can take red, green, blue and alpha values as arguments.
-        //
-        //    Once you have the color object, you should be able to set the view's background color to this object.
-        //
-        + (UIColor *)colorWithRed:(0.78)red green:(0.188)green blue:(0.188)blue alpha:(1)alpha;
-        
-        UIColor *authenticatedBackgroundColor = red;
-        // Additional step to set the above color object to self.view's background color
-        self.view.backgroundColor = self.authenticatedBackgroundColor;
+        self.greetingLabel.text = @"Please provide your Last Name";
+        self.view.backgroundColor = [UIColor redColor];
+        return;
     }
-}
 
+}
 @end
